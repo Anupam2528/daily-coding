d33e1ec36e8f27aeb64f29b877f7f66ed84d773f
Anupam2528 <anupammangale@gmail.com>
2020-10-27 17:10:57 +0530

    Initial Commit of First Rest Service

---
+++ htmlproject/src/test/java/com/anupam/htmlproject/htmlproject/HtmlprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupam.htmlproject.htmlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
